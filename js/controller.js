a0ed834045d0e4a3b8066775f7f1d09c7d81eb5c
chegior <chegiorca@hotmail.com>
2017-06-10 13:52:32 -0600

    connected to the API

--- js/controller.js
+++ js/controller.js
@@ -1,8 +1,21 @@
 angular.module('myApp').controller('myCtrl1',function($scope,mySvc){
   //$scope.myProfile = mySvc.loadMyProfile();
-  $scope.students = mySvc.loadStudents();
+  $scope.loadStudents = function(){
+    mySvc.loadStudents().then(function(response){
+      $scope.students = response;
+    })
 
+  };
+
+  $scope.loadStudents();
 // Verify if a new Student OBJECT has been submited and send it to THE SERVICE
+
+
+
+
+
+
+
   $scope.createProfile = function(student){
     if(!student){
       alert("Error: Please Create a PROFILE, Thanks ")
--- js/service.js
+++ js/service.js
@@ -1,13 +1,29 @@
-angular.module('myApp').service('mySvc',function(){
+angular.module('myApp').service('mySvc',function($http){
 
-  var students = [
-    {name:"Caleb Hicks",tagline:"Learning this Web Project's REST API like a mad man!",bio:"iOS Instructor at DevMountain",profileUrl:"http://calebhicks.com"},
-    {name:"John Stockton",tagline:"Dishing Dimes",bio:"Greatest PG to play the game.",profileUrl:"http://utahjazz.com"},
-    {name:"Karl Malone",tagline:"The Mailman",bio:"2 time NBA MVP",profileUrl:"http://utahjazz.com"},
-    {name:"Jordan",tagline:"Lame tagline",bio:"Lamebio",profileUrl:"profile.url"}
-  ];
+var urlDev = 'https://connections.devmountain.com/api/'
+var students =[];
+
+
+  //
+  // var students = [
+  //   {name:"Caleb Hicks",tagline:"Learning this Web Project's REST API like a mad man!",bio:"iOS Instructor at DevMountain",profileUrl:"http://calebhicks.com"},
+  //   {name:"John Stockton",tagline:"Dishing Dimes",bio:"Greatest PG to play the game.",profileUrl:"http://utahjazz.com"},
+  //   {name:"Karl Malone",tagline:"The Mailman",bio:"2 time NBA MVP",profileUrl:"http://utahjazz.com"},
+  //   {name:"Jordan",tagline:"Lame tagline",bio:"Lamebio",profileUrl:"profile.url"}
+  // ];
+
   this.loadStudents = function(){
-    return students;
+
+    return $http({
+      method:'GET',
+      url:urlDev+'profiles'
+    }).then(function(response){
+      console.log(response);
+      students = response.data.map(function(std){
+        return std; //array of the data from the API
+      })
+      return students;
+    });
   }
 
   this.addStudent = function(student){
